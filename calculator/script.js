c80b5fe9d978d49719d995a6cbcb4a509ca4e034
Abhishek9793ab <abhishek979344ab@gmail.com>
2025-08-29 17:04:51 +0530

    Create script.js

---
+++ calculator/script.js
@@ -0,0 +1,5 @@
+
+let d=document.getElementById("display");
+document.querySelectorAll("button").forEach(b=>b.onclick=()=>{
+   d.value=b.innerText=="="?eval(d.value):b.innerText=="C"?"":d.value+b.innerText
+});
